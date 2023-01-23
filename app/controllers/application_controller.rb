186b33e0b407d874a0961b8d0b58153fb67b153a
Raquel Ciusz <raquel.ciusz@yahoo.com>
2023-01-23 08:45:28 -0300

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   # Pundit: allow-list approach
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # def user_not_authorized
@@ -14,10 +15,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :first_name, :last_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :first_name, :last_name])
   end
 
   private
